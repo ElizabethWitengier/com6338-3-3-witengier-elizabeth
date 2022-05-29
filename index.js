c23e2ee59ce7992f73988da939dca190d5bc74d9
Elizabeth Witengier <ewitengier@ufl.edu>
2022-05-29 12:56:37 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+debugger
+var questionArr = Array
+function runQuiz() {
+  questionsArr = [
+    {
+    questionArr: 'Summer is your favorite season.',
+    answer: true,
+    
+    question: 'Ice cream is your favorite dessert.',
+    answer: true,
+    
+    question: 'You would prefer to be at the beach rather than at the movies.',
+    answer: true,
+    
+    question: 'You would rather be on a boat than a motorcycle.',
+    answer: true,
+    
+    question: 'You play the song Under the Boardwalk by The Drifters every June.',
+    answer: true
+    }
+  ] 
+}
